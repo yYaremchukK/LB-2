6795dab32144d710fdb3a59a566a8c19d0183584
yYaremchukK <Chanchman113339@gmail.com>
2024-11-20 12:50:41 +0200

    Create task4.cpp

---
+++ task4.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int N;
+
+    cout << "Введіть число N: ";
+    cin >> N;
+
+    cout << "Прості числа від 1 до " << N << ": ";
+    for (int i = 1; i <= N; ++i) {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
